7edcc6ef2d9177b6cf5162b8c0a23ccdda339156
Student <DanielNoe@macbook-pro-0877-daniel-noe.local>
2017-04-03 13:18:38 -0400

    MESSAGES

--- Recap.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
+++ Recap.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -34,33 +34,33 @@ canvas.defaultBorderWidth = 10
 canvas.defaultLineWidth = 10
 
 for x in stride(from: 40, to: 500, by: 80){
-// make a cicle - brown - top
-
-canvas.borderColor = Color.init(hue: 26, saturation: 69, brightness: 37, alpha: 100)
-canvas.drawEllipse(centreX: x, centreY: 400, width: 60, height: 60)
-
-
-// make a circle - Orange - top
-canvas.borderColor = Color.init(hue: 22, saturation: 93, brightness: 90, alpha: 100)
-canvas.drawEllipse(centreX: x, centreY: 400, width: 41, height: 41)
-
-// make a circle - Yellow - top
-canvas.borderColor = Color.init(hue: 53, saturation: 93, brightness: 98, alpha: 100)
+    // make a cicle - brown - top
+    
+    canvas.borderColor = Color.init(hue: 26, saturation: 69, brightness: 37, alpha: 100)
+    canvas.drawEllipse(centreX: x, centreY: 400, width: 60, height: 60)
+    
+    
+    // make a circle - Orange - top
+    canvas.borderColor = Color.init(hue: 22, saturation: 93, brightness: 90, alpha: 100)
+    canvas.drawEllipse(centreX: x, centreY: 400, width: 41, height: 41)
+    
+    // make a circle - Yellow - top
+    canvas.borderColor = Color.init(hue: 53, saturation: 93, brightness: 98, alpha: 100)
     canvas.drawEllipse(centreX: x, centreY: 400, width: 21, height: 21)
 }
 
 for y in stride(from: 0, to: 500, by: 80){
-    // make a cicle - brown - top
+    // make a cicle - brown - bottom
     
     canvas.borderColor = Color.init(hue: 26, saturation: 69, brightness: 37, alpha: 100)
     canvas.drawEllipse(centreX: y, centreY: 300, width: 21, height: 21)
     
     
-    // make a circle - Orange - top
+    // make a circle - Orange - bottom
     canvas.borderColor = Color.init(hue: 22, saturation: 93, brightness: 90, alpha: 100)
     canvas.drawEllipse(centreX: y, centreY: 300, width: 41, height: 41)
     
-    // make a circle - Yellow - top
+    // make a circle - Yellow - bottom
     canvas.borderColor = Color.init(hue: 53, saturation: 93, brightness: 98, alpha: 100)
     canvas.drawEllipse(centreX: y, centreY: 300, width: 60, height: 60)
 }
@@ -75,8 +75,20 @@ canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 300, width: 480, height: 100)
 
 
 for x in stride(from: 40, to: 500, by: 80){
+    
+    //yellow lines
     canvas.lineColor = Color.init(hue: 53, saturation: 93, brightness: 98, alpha: 100)
     canvas.drawLine(fromX: x+10, fromY: 301, toX: x+10, toY: 399)
+    canvas.drawLine(fromX: x-10, fromY: 301, toX: x-10, toY: 399)
+    
+    //orange lines
+    canvas.lineColor = Color.init(hue: 22, saturation: 93, brightness: 90, alpha: 100)
+    canvas.drawLine(fromX: x+20, fromY: 301, toX: x+20, toY: 399);canvas.drawLine(fromX: x-20, fromY: 301, toX: x-20, toY: 399)
+    
+    //brown lines
+    canvas.lineColor = Color.init(hue: 26, saturation: 69, brightness: 37, alpha: 100)
+    canvas.drawLine(fromX: x+30, fromY: 301, toX: x+30, toY: 399)
+    canvas.drawLine(fromX: x-30, fromY: 301, toX: x-30, toY: 399)
 }
 
 
